75feead169b241d7f0b9a86fd0ea706c9819de1b
trybhuwanes <trybhuwanes@student.ub.ac.id>
2022-03-13 18:43:42 +0700

    progress 2 update

--- app/src/main/java/com/raion/magang2022/BiodataActivity.kt
+++ app/src/main/java/com/raion/magang2022/BiodataActivity.kt
@@ -8,6 +8,7 @@ import android.widget.Toast
 import com.google.android.material.textfield.TextInputEditText
 
 class BiodataActivity : AppCompatActivity() {
+    //Mendeklarasikan
     private lateinit var inputNama : TextInputEditText
     private lateinit var inputUsia : TextInputEditText
     private lateinit var inputTinggi : TextInputEditText
@@ -17,18 +18,20 @@ class BiodataActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_biodata)
 
+        //Mendefinisikan objek
         inputNama = findViewById(R.id.nama_lengkap)
         inputUsia = findViewById(R.id.usia)
         inputTinggi = findViewById(R.id.tinggi_badan)
         btnSelesai = findViewById(R.id.btn_selesai)
 
-
+        //Fungsi ketika tombol selesai diklik
         btnSelesai.setOnClickListener {
+            //Mengubahnya menjadi text agar bisa ditampilkan dan dibaca oleh user
             val namaLengkap = inputNama.text.toString().trim()
             val usia = inputUsia.text.toString().trim()
             val tinggiBadan = inputTinggi.text.toString().trim()
 
-
+            //Pengecekan inputan
             if (namaLengkap.isEmpty()) {
                 inputNama.error = "Masukkan nama lengkap Anda"
                 return@setOnClickListener
@@ -38,6 +41,7 @@ class BiodataActivity : AppCompatActivity() {
             } else if (tinggiBadan.isEmpty()) {
                 inputTinggi.error = "Masukkan tinggi badan Anda"
             } else {
+                //Intent untuk passing data
                 val intent = Intent(this, ProfilActivity::class.java)
                 intent.putExtra("Nama", namaLengkap)
                 intent.putExtra("Usia", usia)
--- app/src/main/java/com/raion/magang2022/ProfilActivity.kt
+++ app/src/main/java/com/raion/magang2022/ProfilActivity.kt
@@ -11,16 +11,18 @@ class ProfilActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profil)
 
-
+        //Mengambil data
         val intent = intent
         val namaLengkap = intent.getStringExtra("Nama")
         val usia = intent.getStringExtra("Usia")
         val tinggiBadan = intent.getStringExtra("Tinggi")
 
+        //Mendefinisikan objek sebagai tempat menampilkan teks biodata
         val tampilNama = findViewById<TextView>(R.id.tv_nama)
         val tampilUsia = findViewById<TextView>(R.id.tv_usia)
         val tampilTinggi = findViewById<TextView>(R.id.tv_tinggiBadan)
 
+        //Menampilkan data
         tampilNama.text = namaLengkap
         tampilUsia.text = "Usia\n$usia"
         tampilTinggi.text = "Tinggi\nBadan\n$tinggiBadan"
